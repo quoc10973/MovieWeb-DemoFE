5703d5a75feb2984bbfa645973509b2cac8bde1a
quoc10973 <rumble01667057271@gmail.com>
2024-10-24 05:45:33 +0700

    map data

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
+import imgTemp from '../assets/poster.jpg'
 
-const MovieList = ({ title }) => {
+const MovieList = ({ title, data }) => {
   return (
-    <div className='text-white p-10 mb-8'>
+    <div className='text-white p-10 mb-3'>
         <h2 className='uppercase text-xl mb-4'> {title} </h2>
         <div className='flex items-center space-x-4'>
-            <div className='w-[200px] h-[280px] bg-red-400'>
-
+        {data.length > 0 && data.map((item) => (
+            <div key={item.id} className='flex items-center space-x-4'>
+                <div className='w-[230px] h-[320px] relative group cursor-pointer'>
+                  <div className='group-hover:scale-105 transition-transform duration-500 ease-in-out  bg-cyan-900 rounded-sm w-full h-full'>
+                    <div className='absolute w-full h-[290px] top-0 left-0 bg-black/20'/>
+                      <img src={`${import.meta.env.VITE_IMG_URL}${item.poster_path}`} alt={item.title} className='w-full h-[290px] object-cover'/>
+                      <p className='uppercase flex items-end justify-center text-lg'> {item.title || item.original_title} </p> 
+                   </div>             
+                </div>
             </div>
+        ))
+        }
         </div>
+ 
     </div>
   )
 }
